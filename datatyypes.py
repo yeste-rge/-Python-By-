10a566aab8a256a2d5f462b6ebfdced372c2bb69
yeste-rge <Cheongziqian20@gmail.com>
2023-02-16 11:40:02 +0800

    Create datatyypes.py

---
+++ datatyypes.py
@@ -0,0 +1,10 @@
+午餐 = 55
+晚餐 = 500
+一周餐费 = (午餐 + 晚餐)*7
+print(一周餐费)
+#print(type(一周餐费))<class 'int'"integer整数">
+
+月生活费 = 5000
+日平均 = 月生活费 // 30
+print(日平均)
+#print(type(日平均))<class 'float'"小数/浮点数">
