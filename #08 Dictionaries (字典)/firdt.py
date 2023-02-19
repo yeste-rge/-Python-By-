0503f1b3197d306a9abf7dfb4038d8f16557ec14
yeste-rge <Cheongziqian20@gmail.com>
2023-02-19 10:36:45 +0800

    Update firdt.py

--- #08 Dictionaries (字典)/firdt.py
+++ #08 Dictionaries (字典)/firdt.py
@@ -1,2 +1,16 @@
 #Python 零基礎新手入門 #08 Dictionaries (字典)
- 
+#Item(项目)[Key(键) : Value(值)] Key和Value之间是由冒号所隔阂 而不同Items之间则是由逗号相隔
+#注意字典中每个Key都必须是独一无二的
+#Dictionary适合拿来储存系统性的资讯
+手机 = {
+    "厂牌" : "苹果",
+    "型号" : "iPhonr 14 Pro",
+    "容量" : "1TB"
+}
+手机["颜色"] = ["深紫色","银色","金色","黑色"]
+print(手机)
+#print(手机.ge t("颜色","查无此资讯"))查无此资讯
+#if "型号" in 手机:
+    #print(手机["型号"])iPhonr 14 Pro
+#else:
+    #print("查无此资讯")
