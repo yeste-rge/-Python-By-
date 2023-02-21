8e90b18d226b623ff49ea4e0378145ab1428152b
yeste-rge <Cheongziqian20@gmail.com>
2023-02-21 18:48:51 +0800

    Update random.py

--- #09 Module (模組)/random.py
+++ #09 Module (模組)/random.py
@@ -1,7 +1,16 @@
 #Python 零基礎新手入門 #09 Module (模組)
 #random随机
+#import random
+#数字 = [1,2,3,4,5,6]
+#random.shuffle(数字) #shuffle(洗牌)函式
+#print(数字)
+
+#import random
+#数字 = [1,2,3,4,5,6]
+#a = random.choice(数字) #choice(选择)
+#print(a) 
+
 import random
 数字 = [1,2,3,4,5,6]
-random.shuffle(数字)
-#shuffle(洗牌)函式
-print(数字)
+a = random.randint(10,50)#randint(random integer)的函式
+print(a)
