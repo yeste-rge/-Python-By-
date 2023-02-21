ff10238c5c07900a5de9ec08a9578715c2ae6ec7
yeste-rge <Cheongziqian20@gmail.com>
2023-02-21 22:17:37 +0800

    Update random.py

--- #09 Module (模組)/random.py
+++ #09 Module (模組)/random.py
@@ -14,10 +14,14 @@
 #数字 = [1,2,3,4,5,6]
 #a = random.randint(10,50)#randint(random integer)的函式
 #print(a)
-
+#应用练习:密码产生器
 import random
 import string #string(模组)它有一些与文字相关的语法
 数字 = string.digits #digits可以产生0到9数字
 英文 = string.ascii_letters #ascii_letters可以产生英文小写和大写的字母
-字母表 = 数字 + 英文
-print(字母表)
+字母表 = list(数字 + 英文)#用list语法先把它转成清单(list)
+random.shuffle(字母表)#shuffle()方法将序列的所有元素随机排序。
+长度 = int(input("你的密码要几位数?"))
+密码 = "".join(字母表[:长度])#join语法把它转成一般的字串
+#密码 = 字母表[:6]#截取出前面6位字母
+print(f"你的密码是:{密码}")
