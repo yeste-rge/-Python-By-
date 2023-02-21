0eabc406c77496b04e582a47c491afa3a2d2744c
yeste-rge <Cheongziqian20@gmail.com>
2023-02-21 19:00:03 +0800

    Update random.py

--- #09 Module (模組)/random.py
+++ #09 Module (模組)/random.py
@@ -10,7 +10,13 @@
 #a = random.choice(数字) #choice(选择)
 #print(a) 
 
+#import random
+#数字 = [1,2,3,4,5,6]
+#a = random.randint(10,50)#randint(random integer)的函式
+#print(a)
+
 import random
-数字 = [1,2,3,4,5,6]
-a = random.randint(10,50)#randint(random integer)的函式
-print(a)
+import string #string(模组)它有一些与文字相关的语法
+数字 = string.digits #digits可以产生0到9数字
+英文 = string.ascii_letters #ascii_letters可以产生英文小写和大写的字母
+字母
