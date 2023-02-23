5e777ceb9437ecd6c69b000d2f09cc1e339a60c0
yeste-rge <Cheongziqian20@gmail.com>
2023-02-23 16:06:03 +0800

    Delete modul.py

--- #09 Module (模組)/modul.py
+++
@@ -1,9 +0,0 @@
-#Python 零基礎新手入門 #09 Module (模組)
-#import 模组名称 as 昵称
-import my_module as hi
-#hi.打招呼("小明")小明你好~
-#from 模组名称 import 函式名称1,函式名称2...
-from my_module import 打招呼
-打招呼("阿杰")
-#模组名称或别名.函式名称(参数资料)
-#模组名称或别名.变数名称
