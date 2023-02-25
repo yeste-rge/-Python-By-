cf5cc1a66cc8bc9685746e10aa9b8b5ef55f10ea
yeste-rge <Cheongziqian20@gmail.com>
2023-02-25 16:46:04 +0800

    Update main.py

--- 彭彭/#12 Python Package 封包的設計與使用/main.py
+++ 彭彭/#12 Python Package 封包的設計與使用/main.py
@@ -1,4 +1,7 @@
 #主程式
-import geometry.point #geometry.point(模组的元整名称要包含它所属代封包)
+import geometry.point #geometry.point是模组的元整名称要包含它所属的封包(在代码中使用point模块中定义的函数、类或者变量等)
 result=geometry.point.distance(3,4)
-print(result)
+print("距离",result)
+import geometry.line as line
+result=line.slope(1,1,3,3) #(载入线段模组)
+print("斜率",result)
