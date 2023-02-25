4802f578ae1edc861beffbedfecc6cbc8d7768f9
yeste-rge <Cheongziqian20@gmail.com>
2023-02-25 22:50:20 +0800

    Delete main.py

--- 彭彭/#12 Python Package 封包的設計與使用/main.py
+++
@@ -1,7 +0,0 @@
-#主程式
-import geometry.point #geometry.point是模组的元整名称要包含它所属的封包(在代码中使用point模块中定义的函数、类或者变量等)
-result=geometry.point.distance(3,4)
-print("距离",result)
-import geometry.line as line
-result=line.slope(1,1,3,3) #(载入线段模组)
-print("斜率",result)
