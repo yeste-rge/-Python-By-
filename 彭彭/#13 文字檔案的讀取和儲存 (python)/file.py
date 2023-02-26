9dfc5c6327266992e660dcd7f4c7e96f5c67b86e
yeste-rge <Cheongziqian20@gmail.com>
2023-02-26 10:16:47 +0800

    Update file.py

--- 彭彭/#13 文字檔案的讀取和儲存 (python)/file.py
+++ 彭彭/#13 文字檔案的讀取和儲存 (python)/file.py
@@ -1,7 +1,8 @@
 #储存档案
-file=open("data.txt",mode="w" , encoding="utf-8") #开启
-file.write("测试中文\n好棒棒") #操作
-file.close() #关闭
-#读取答案
-
+#file=open("data.txt",mode="w" , encoding="utf-8") #开启
+#file.write("测试中文\n好棒棒") #操作
+#file.close() #关闭
+with open("data.txt" , mode="w" , encoding="utf-8") as file:
+    file.write("测试中文\n好棒棒")
+#读取档案
 #使用 JSON 格式读取、腹泻档案
