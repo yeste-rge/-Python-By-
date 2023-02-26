fb410086fe4e881e8d5c589db01198c4c4dcd09c
yeste-rge <Cheongziqian20@gmail.com>
2023-02-26 11:32:33 +0800

    Update file.py

--- 彭彭/#13 文字檔案的讀取和儲存 (python)/file.py
+++ 彭彭/#13 文字檔案的讀取和儲存 (python)/file.py
@@ -2,16 +2,17 @@
 #file=open("data.txt",mode="w" , encoding="utf-8") #开启
 #file.write("测试中文\n好棒棒") #操作
 #file.close() #关闭
-with open("data.txt" , mode="w" , encoding="utf-8") as file:
-    file.write("5\n3")
+#with open("data.txt" , mode="w" , encoding="utf-8") as file:
+    #file.write("5\n3")
 #读取档案
 #with open("data.txt" , mode="r" , encoding="utf-8") as file:
 #    data=file.read()
 #print(data)
 #把档案中的数字资料，一行一行的读取出来，并计算总合
-sum=0
-with open("data.txt" , mode="r" , encoding="utf-8") as file:
-    for line in file:  #一行一行的读取
-        sum+=int(line) #把line的文字转换成数字
-print(sum)
+#sum=0
+#with open("data.txt" , mode="r" , encoding="utf-8") as file:
+    #for line in file:  #一行一行的读取
+        #sum+=int(line) #把line的文字转换成数字
+#print(sum)
+ 
 #使用 JSON 格式读取、腹泻档案
