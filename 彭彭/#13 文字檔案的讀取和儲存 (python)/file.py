1cac04aec8b4fec8f9a4b0eabf0c1615e2eed138
yeste-rge <Cheongziqian20@gmail.com>
2023-03-10 17:52:25 +0800

    Update file.py

--- 彭彭/#13 文字檔案的讀取和儲存 (python)/file.py
+++ 彭彭/#13 文字檔案的讀取和儲存 (python)/file.py
@@ -2,16 +2,16 @@
 #file=open("data3.txt",mode="w",encoding="utf-8") #开启
 # file.write("测试中文\n好棒棒") #操作
 # file.close()#关闭
-with open("data5.txt",mode="w",encoding="utf-8") as file: #with不需自己close 它会"自动","安全","可靠"把档案释放和关闭(ta会比计较好)
-    file.write("5\n3") 
+# with open("data5.txt",mode="w",encoding="utf-8") as file: #with不需自己close 它会"自动","安全","可靠"把档案释放和关闭(ta会比计较好)
+#    file.write("5\n3") 
 
 #读取档案
 #把档案中的数字资料，一行一行的读取出来，并计算总合
-sum=0
-with open("data5.txt", mode="r", encoding="utf-8") as file:
-    for line in file: #一行一行的读取
-        sum+=int(line) #line转换成int(整数)加到总和
-print(sum) #把总和
+# sum=0
+# with open("data5.txt", mode="r", encoding="utf-8") as file:
+#    for line in file: #一行一行的读取
+#        sum+=int(line) #line转换成int(整数)加到总和
+# print(sum) #把总和印出
 
 #with open("data.txt" , mode="r" , encoding="utf-8") as file:
     #for line in file:  #一行一行的读取
@@ -19,5 +19,9 @@ print(sum) #把总和
 #print(sum)
  
 #使用 JSON 格式读取、腹泻档案
-
+import json
+with open("config.json", mode="r") as file:
+    data=json.load(file)
+print("name", data["name"])
+print("version", data["version"])
 
