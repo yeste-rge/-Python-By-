4f818c8e9e0e0f22bd91fbc249ac78e746cb889e
yeste-rge <Cheongziqian20@gmail.com>
2023-03-10 17:07:28 +0800

    Update file.py

--- 彭彭/#13 文字檔案的讀取和儲存 (python)/file.py
+++ 彭彭/#13 文字檔案的讀取和儲存 (python)/file.py
@@ -4,13 +4,15 @@
 # file.close()#关闭
 with open("data5.txt",mode="w",encoding="utf-8") as file: #with不需自己close 它会"自动","安全","可靠"把档案释放和关闭(ta会比计较好)
     file.write("5\n3") 
- 
+
 #读取档案
-# with open("data4.txt", mode="r", encoding="utf-8") as file:
-#    data=file.read()
-#print(data)
 #把档案中的数字资料，一行一行的读取出来，并计算总合
-#sum=0
+sum=0
+with open("data5.txt", mode="r", encoding="utf-8") as file:
+    for line in file: #一行一行的读取
+        sum+=int(line) #line转换成int(整数)加到总和
+print(sum) #把总和
+
 #with open("data.txt" , mode="r" , encoding="utf-8") as file:
     #for line in file:  #一行一行的读取
         #sum+=int(line) #把line的文字转换成数字
