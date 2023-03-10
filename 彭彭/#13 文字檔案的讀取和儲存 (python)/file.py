d1f607428ece50953925aca1e9c21a0417a578ca
yeste-rge <Cheongziqian20@gmail.com>
2023-03-10 16:37:33 +0800

    Update file.py

--- 彭彭/#13 文字檔案的讀取和儲存 (python)/file.py
+++ 彭彭/#13 文字檔案的讀取和儲存 (python)/file.py
@@ -2,13 +2,12 @@
 #file=open("data3.txt",mode="w",encoding="utf-8") #开启
 # file.write("测试中文\n好棒棒") #操作
 # file.close()#关闭
-with open("data4.txt",mode="w",encoding="utf-8") as file:
-    file.write("测试中文\n好棒棒")
- #with会自动 安全 可靠 把档案释放和关闭
+with open("data4.txt",mode="w",encoding="utf-8") as file: #with会自动 安全 可靠 把档案释放和关闭
+    file.write("测试中文\n好棒棒") 
+ 
 #读取档案
-#with open("data.txt" , mode="r" , encoding="utf-8") as file:
-#    data=file.read()
-#print(data)
+with open("data4.txt", mode="r", encoding="utf-8") as file:
+    data=file.read()
 #把档案中的数字资料，一行一行的读取出来，并计算总合
 #sum=0
 #with open("data.txt" , mode="r" , encoding="utf-8") as file:
