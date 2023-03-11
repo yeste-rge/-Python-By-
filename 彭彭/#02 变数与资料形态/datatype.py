506461ebfcd6ee13ca83f4f96bfacdcb4a84dd02
yeste-rge <Cheongziqian20@gmail.com>
2023-03-11 12:17:14 +0800

    Create datatype.py

---
+++ 彭彭/#02 变数与资料形态/datatype.py
@@ -0,0 +1,36 @@
+# 资料: 程式的版本单位
+
+# 数字(int)
+## exm.
+### 3456
+### 3.5
+
+# 字串(string)
+"测试中文"
+"Hello world"
+
+# 布林值(Boolean)
+## exm.
+### True
+### False
+
+# 有顺序、可动的列表 List
+### [3,4,5]
+###["Hello","World"]
+
+#有顺序、不可动的列表 Tuple
+### (3,4,5)
+### ("Hello","World")
+
+#集合 Set
+### {3,4,5}
+### {"Hello","World"}
+
+#字典 Dictionary
+#### {"Apple":"苹果","data":"资料"}
+
+# 变数: 用来储存资料的自名称
+# 变数名称=资料
+x=3
+# print(资料)
+print(3)
