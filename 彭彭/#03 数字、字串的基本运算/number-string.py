54a73bc40f5ab9b97ac2457116f95b16c1cc862a
yeste-rge <Cheongziqian20@gmail.com>
2023-03-15 20:45:47 +0800

    Update number-string.py

--- 彭彭/#03 数字、字串的基本运算/number-string.py
+++ 彭彭/#03 数字、字串的基本运算/number-string.py
@@ -1,6 +1,35 @@
 # 数字运算
 # x=7/6
 #print(x) # a/b 是小数除法 # a//b 是整数除法
-x=2**3
-print(x) #开根号(Punca kuasa dua)
+
+# x=3//6
+# print(x) # a/b 是小数除法 # a//b 是整数除法
+
+# x=2**3
+# print(x) 
+# x=2**0.5 #开根号(Punca kuasa dua)
+# print(x) 
+#开根号(Punca kuasa dua)
+# exm.
+# √9 = 3
+# √4 = 2
+
+# x=7%3 #跟 a/b 是相同概念
+# print(x)
+
+# x=2+3
+# print(x)
+# x=x+1 
+# x+=1 #x=x+1 #将变数中的数字 +1
+# x+1 是拿上面的'print(x)'(在"line 21")再加 1
+#because 5 + 1 等于 6 so 然后再覆盖回变数x
+# x-=1 #x=x-1 #将变数中的数字 -1
+# x*=1 #x=x*1 #将变数中的数字 *1
+# print(x)
+
 # 字串运算
+# s="Hello" #也相同 s='Hello'
+# s="Hell\"o" # \ 的名叫做"跳脱"跟外面做区隔
+# s="Hello"  "World" #"Hello" + "World"
+s="Hello\nWorld" #\n 代表"换行"
+print(s)
