d7b7e8abbcd5b9e8f1da689c34a0beedd038e982
yeste-rge <Cheongziqian20@gmail.com>
2023-03-16 14:42:43 +0800

    Update list-tuple.py

--- 彭彭/#04 有序列表的基本运算 - List , Tuple/list-tuple.py
+++ 彭彭/#04 有序列表的基本运算 - List , Tuple/list-tuple.py
@@ -1,6 +1,19 @@
 # 有序可变动列表 List
-grades=[12,60,15,70,90]
-print(grades[1:4]) # 代表我取得列表中的第 1 到 3 个编号 不包含 4 个编号
+# grades=[12,60,15,70,90]
+# grades[1:4]=[] # 连续删除列表中从编号 1 到编号 4(不包含) 的资料 
+#把 60,15,70 删除并变成空白 整个列表就只剩 12 和 90
+
+# grades=grades+[12,33] #在原本的列表中加 12 和 33
+# length=len(grades) # 取得列表的长度 len(列表资料)
+# print(length)
 # grades[0]=55 #把 55 放到列表中的第一个位置
 # print(grades[0])
+
+# data=[[3,4,5],[6,7,8]]
+# print(data)
+# data[0][0:2]=[5,5,5]
+# print(data)
 # 有序不可变动列表 Tuple
+data=(3,4,5)
+# data[0]=5 #错误: Tuple 的资料不可以变动
+print(data[0:2])
