4b00328142b1e41513452f9f4b2d1b21f49ecf77
yeste-rge <Cheongziqian20@gmail.com>
2023-04-20 22:28:37 +0800

    Update loop-basic.py

--- 彭彭/#07流程控制：迴圈基礎，while 迴圈、for 迴圈/loop-basic.py
+++ 彭彭/#07流程控制：迴圈基礎，while 迴圈、for 迴圈/loop-basic.py
@@ -1,9 +1,12 @@
 # while 迴圈
 #1+2+3...+10
-n=1
-sum=0 #记录累加的结果
-while n<=10: 
-    sum=sum+n
-    n+=1 
-print(sum)#把n的资料加1
+# n=1
+# sum=0 #记录累加的结果
+# while n<=10: 
+#     sum=sum+n
+#     n+=1 
+# print(sum)#把n的资料加1
+
 # for 迴圈
+for x in [3,5,1]:
+    print(x)
