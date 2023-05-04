dd62debe3be20aadeef51c3130fb748a7f5ee3f0
yeste-rge <Cheongziqian20@gmail.com>
2023-05-04 23:51:51 +0800

    Create function-basic.py

---
+++ 彭彭/#09 函式基礎：定義並呼叫函式/function-basic.py
@@ -0,0 +1,26 @@
+# 定义函式
+
+# 函式内部的程式码,若是没有做函式呼叫,就不会执行
+# def multiply():
+#     print(3*4)
+
+# def multiply(n1,n2):
+#     return n1*n2
+
+# 呼叫函式
+
+# value=multiply(3,4)+multiply(10,5) # (3+4)+(10+5)
+# print(value)
+
+
+# multiply(3,4)
+# multiply(10,8)
+
+# 程式的包装
+def calculate(max):
+    sum=0
+    for x in range(1,max+1): # 1+2+3..+10
+        sum=sum+x
+    print(sum)
+calculate(10)
+calculate(20)
