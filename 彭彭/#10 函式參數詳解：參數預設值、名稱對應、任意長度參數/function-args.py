f032503eaadcd9e4a441db19b843609bb872ad72
yeste-rge <Cheongziqian20@gmail.com>
2023-05-12 10:20:14 +0800

    Update function-args.py

--- 彭彭/#10 函式參數詳解：參數預設值、名稱對應、任意長度參數/function-args.py
+++ 彭彭/#10 函式參數詳解：參數預設值、名稱對應、任意長度參數/function-args.py
@@ -5,8 +5,17 @@
 # power(4)
 
 # 使用参数名称对应
-def divide(n1,n2):
-    print(n1/n2)
-divide(2,4)
-divide(n2=2,n1=4)
-# 无限参数资料
+# def divide(n1,n2):
+#     print(n1/n2)
+# divide(2,4)
+# divide(n2=2,n1=4)
+
+# 无限/不定 参数资料
+def avg(*ns):
+    sum=0
+    for n in ns:
+        sum+=n
+    print(sum/len(ns))
+avg(3,4)
+avg(3,5,10)
+avg(1,4,-1,-8)
