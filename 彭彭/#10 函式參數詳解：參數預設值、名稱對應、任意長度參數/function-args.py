88448bcc5fd37dcfc53a24ca0a5c9af73df13c6c
yeste-rge <Cheongziqian20@gmail.com>
2023-05-12 09:54:25 +0800

    Create function-args.py

---
+++ 彭彭/#10 函式參數詳解：參數預設值、名稱對應、任意長度參數/function-args.py
@@ -0,0 +1,12 @@
+# 参数的预设资料
+# def power(base,exp=0): #base 是 kuasa tiga(三个平方) #exp 是 kuasa dua(两个平方)
+#     print(base**exp) # 一个 * 是乘法 两个 **是开方(kuasa)
+# power(3,2)
+# power(4)
+
+# 使用参数名称对应
+def divide(n1,n2):
+    print(n1/n2)
+divide(2,4)
+divide(n2=2,n1=4)
+# 无限参数资料
